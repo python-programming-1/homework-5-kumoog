b7d6e8c29902c05290b68710b6cabf3182d29601
Boyoon chung <kumoog@naver.com>
2019-05-09 23:35:02 -0400

    add parties.py

--- parties.py
+++ parties.py
@@ -1,6 +1,6 @@
 import csv
 import pprint
-import operator
+
 
 
 def get_bar_party_data():
@@ -34,84 +34,73 @@ def get_most_noisy_city_and_borough(data):
     noisiest_city_and_borough = {'city': data[1].get('city'), 'borough': data[1].get('borough'),
                                  'num_city_calls': data[1].get('num_calls'),
                                  'num_borough_calls': data[1].get('num_calls')}
-    b ={}
-    h ={}
+    city ={}
+    borough ={}
 
     for i in range(1,2441):
-        if not data[i].get('city') in b:
-           b.setdefault(data[i].get('city'), int(data[i].get('num_calls')))
-
+        if not data[i].get('city') in city:
+           city.setdefault(data[i].get('city'), int(data[i].get('num_calls')))
            continue
-
-        elif data[i].get('city') in b:
-           y = (data[i].get('city'))
-           t = int(b.get(data[i].get('city'))) + int(data[i].get('num_calls'))
-           c = {y: t}
-           b.update(c)
+        elif data[i].get('city') in city:
+           update_city = (data[i].get('city'))
+           update_city_numCall = int(city.get(data[i].get('city'))) + int(data[i].get('num_calls'))
+           updateCityCityCall = {update_city: update_city_numCall}
+           city.update(updateCityCityCall)
            continue
-
-
 
     for i in range(1,2441):
-        if data[i].get('city') == max(b, key=lambda key: b[key]):
-            if not data[i].get('borough') in h:
-                h.setdefault(data[i].get('borough'),data[i].get('num_calls'))
+        if data[i].get('city') == max(city, key=lambda key: city[key]):
+            if not data[i].get('borough') in borough:
+                borough.setdefault(data[i].get('borough'),data[i].get('num_calls'))
                 continue
-            elif data[i].get('borough') in h:
-                q = (data[i].get('borough'))
-                w = int(h.get(data[i].get('borough'))) + int(data[i].get('num_calls'))
-                j = {q: w}
-                h.update(j)
+            elif data[i].get('borough') in borough:
+                update_borough = (data[i].get('borough'))
+                update_borough_numCall = int(borough.get(data[i].get('borough'))) + int(data[i].get('num_calls'))
+                updateBoroughBoroughCall = {update_borough: update_borough_numCall}
+                borough.update(updateBoroughBoroughCall)
                 continue
 
-
-    noisiest_city_and_borough = {'city': max(b, key=lambda key: b[key]), 'borough': max(h, key=lambda key: h[key]),
-                                                                         'num_city_calls': max(b.values()),
-                                                                         'num_borough_calls': max(h.values())}
+    noisiest_city_and_borough = {'city': max(city, key=lambda key: city[key]), 'borough': max(borough, key=lambda key: borough[key]),
+                                                                         'num_city_calls': max(city.values()),
+                                                                         'num_borough_calls': max(borough.values())}
 
     return noisiest_city_and_borough
 
 
 def get_quietest_city_and_borough(data):
     """ fill in the Nones for the dictionary below using the bar party data """
-    quietest_city_and_borough = {'city': data[1].get('city'), 'borough': data[1].get('borough'),
+    noisiest_city_and_borough = {'city': data[1].get('city'), 'borough': data[1].get('borough'),
                                  'num_city_calls': data[1].get('num_calls'),
                                  'num_borough_calls': data[1].get('num_calls')}
-    b ={}
-    h ={}
+    city ={}
+    borough ={}
 
     for i in range(1,2441):
-        if not data[i].get('city') in b:
-           b.setdefault(data[i].get('city'), int(data[i].get('num_calls')))
-
+        if not data[i].get('city') in city:
+           city.setdefault(data[i].get('city'), int(data[i].get('num_calls')))
            continue
-
-        elif data[i].get('city') in b:
-           y = (data[i].get('city'))
-           t = int(b.get(data[i].get('city'))) + int(data[i].get('num_calls'))
-           c = {y: t}
-           b.update(c)
+        elif data[i].get('city') in city:
+           update_city = (data[i].get('city'))
+           update_city_numCall = int(city.get(data[i].get('city'))) + int(data[i].get('num_calls'))
+           updateCityCityCall = {update_city: update_city_numCall}
+           city.update(updateCityCityCall)
            continue
-
-
 
     for i in range(1,2441):
-        if data[i].get('city') == min(b, key=lambda key: b[key]):
-            if not data[i].get('borough') in h:
-                h.setdefault(data[i].get('borough'),data[i].get('num_calls'))
+        if data[i].get('city') == min(city, key=lambda key: city[key]):
+            if not data[i].get('borough') in borough:
+                borough.setdefault(data[i].get('borough'),data[i].get('num_calls'))
                 continue
-            elif data[i].get('borough') in h:
-                q = (data[i].get('borough'))
-                w = int(h.get(data[i].get('borough'))) + int(data[i].get('num_calls'))
-                j = {q: w}
-                h.update(j)
+            elif data[i].get('borough') in borough:
+                update_borough = (data[i].get('borough'))
+                update_borough_numCall = int(borough.get(data[i].get('borough'))) + int(data[i].get('num_calls'))
+                updateBoroughBoroughCall = {update_borough: update_borough_numCall}
+                borough.update(updateBoroughBoroughCall)
                 continue
 
-
-    quietest_city_and_borough = {'city': min(b, key=lambda key: b[key]), 'borough': min(h, key=lambda key: h[key]),
-                                                                         'num_city_calls': min(b.values()),
-                                                                         'num_borough_calls': min(h.values())}
-
+    quietest_city_and_borough = {'city': min(city, key=lambda key: city[key]), 'borough': min(borough, key=lambda key: borough[key]),
+                                                                         'num_city_calls': min(city.values()),
+                                                                         'num_borough_calls': min(borough.values())}
     return quietest_city_and_borough
 
 
